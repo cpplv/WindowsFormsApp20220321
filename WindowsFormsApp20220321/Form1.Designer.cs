0fc74befc24a3f04b7cff50dcbcca192ba1d64a6
cpplv <aliepins@inbox.lv>
2022-03-21 19:42:17 +0200

    Sveiks

--- WindowsFormsApp20220321/Form1.Designer.cs
+++ WindowsFormsApp20220321/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(327, 285);
+            this.button1.Location = new System.Drawing.Point(226, 197);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(320, 111);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Click to change";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- WindowsFormsApp20220321/Form1.cs
+++ WindowsFormsApp20220321/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp20220321
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Sveiks!";
+        }
     }
 }
